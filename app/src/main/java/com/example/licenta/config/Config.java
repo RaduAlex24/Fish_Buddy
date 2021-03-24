15ba20dadfd5e7a1fea1cb8c283b8abc9bcdf60f
RaduAlex24 <72069064+RaduAlex24@users.noreply.github.com>
2021-03-24 18:48:15 +0200

    Delete app/src/main/java/com/example/licenta/config directory

--- app/src/main/java/com/example/licenta/config/Config.java
+++
@@ -1,8 +0,0 @@
-package com.example.licenta.config;
-
-public class Config {
-    public static final String url = "jdbc:oracle:thin:@192.168.100.30:1521:XE";
-    public static final String driver = "oracle.jdbc.driver.OracleDriver";
-    public static final String nume = "SYSTEM";
-    public static final String parola = "ALLU1234";
-}
