412b112e94b45be19b92dea64460149e0b66c911
PopescuLuca <popesculuca18@stud.ase.ro>
2021-04-18 16:31:33 +0300

    inceput burger meniu

--- app/src/main/java/com/example/licenta/MainActivity.java
+++ app/src/main/java/com/example/licenta/MainActivity.java
@@ -1,27 +1,22 @@
 package com.example.licenta;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
-import com.example.licenta.clase.forum.CategoryForum;
-import com.example.licenta.clase.forum.ForumPost;
 import com.example.licenta.clase.user.CurrentUser;
-import com.example.licenta.database.ConexiuneBD;
 import com.example.licenta.homeFragments.ViewPagerAdapter;
-import com.example.licenta.util.dateUtils.DateConverter;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private long mBackPressed;
 
     private CurrentUser currentUser = CurrentUser.getInstance();
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +41,23 @@ public class MainActivity extends AppCompatActivity {
 
         // Initializare pager adapter
         initViewPagerAdapter();
+        configNavigation();
+    }
+
+    private void configNavigation() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        TextView tvUsername = findViewById(R.id.tv_main_username);
+        TextView tvEmail = findViewById(R.id.tv_main_email);
     }
 
     // Functii
@@ -51,8 +65,8 @@ public class MainActivity extends AppCompatActivity {
     private void initComponents() {
         viewPager = findViewById(R.id.viewPager_main);
         fragmentManager = getSupportFragmentManager();
-    }
 
+    }
 
     // Setare adapter
     private void initViewPagerAdapter() {
