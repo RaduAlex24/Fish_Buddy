6202e83b90bfd202ae178ee6523a69a1a0b93e12
Alex <radualexandru18@stud.ase.ro>
2021-07-16 21:03:37 +0300

    Rezolvare nnisgh mode si finalizare tutorial

--- app/src/main/java/com/example/licenta/introTutorialSlider/IntroTutorialSlider.java
+++ app/src/main/java/com/example/licenta/introTutorialSlider/IntroTutorialSlider.java
@@ -25,31 +25,41 @@ public class IntroTutorialSlider extends AppIntro2 {
         // General
         addSlide(AppIntroFragment.newInstance("Modul forum",
                 "Acest modul permite interactiunea cu alti pescari, prin principiul de intrebare raspuns, pe diferite teme.",
-                R.drawable.forum, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.forum_general, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
         // Interactiune cu butoanele
         addSlide(AppIntroFragment.newInstance("Modul forum",
                 "Poti interactiona cu orice postare prin intermediul butoaneleor de pe aceasta. Poti aprecia sau nu postare, iar" +
                         " prin butonul in forma de inima o poti adauga la favorite, pentru a o regasi mai usor.",
-                R.drawable.forum, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.forum_postare, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
         // Forum detaliat
         addSlide(AppIntroFragment.newInstance("Modul forum",
                 "Daca vrei sa citesti toata postarea forum poti da click pe ea si vei fi condus la pagina ei. " +
                         "In aceasta pagina poti adauga si citi comentariile postarii.",
-                R.drawable.forum, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.forum_detalii_postare, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
         // Sortari
         addSlide(AppIntroFragment.newInstance("Modul forum",
                 "Cele doua butoane permit sortarea tuturor postarilor. Unul dintre acestea sorteaza in " +
                         "functie de categorii, iar celalalt in functie de diferite crieterii precum numar puncte, numar comentarii, etc.",
-                R.drawable.forum, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.forum_sortare, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
         // Adaugare postare
         addSlide(AppIntroFragment.newInstance("Modul forum",
                 "Butonul plus te duce la pagina de creare a unei postari. Dupa ce completezi toate campurile, " +
                         "postarea devine vizibila pentru toata lumea.",
-                R.drawable.forum, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.forum_adaugare_postare, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
 
         // Asistet virtual
@@ -57,21 +67,27 @@ public class IntroTutorialSlider extends AppIntro2 {
         addSlide(AppIntroFragment.newInstance("Modul asistent virtual",
                 "Acest modul iti permite sa vorbesti direct cu robotul aplicatie noastre, ce" +
                         " are rolul de ati raspunde la orice intrebare.",
-                R.drawable.b, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.asistent_virtual_general, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
 
         // Redirectionari
         addSlide(AppIntroFragment.newInstance("Modul asistent virtual",
                 "Principalul scop al asistentului este sa te duca unde ai nevoie, " +
                         " asa ca daca ii sugerezi o intentie a ta, te va redirectiona la pagina dorita.",
-                R.drawable.b, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.asistent_redirectionare, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
 
-        // Redirectionari
+        // Cautari
         addSlide(AppIntroFragment.newInstance("Modul asistent virtual",
                 "De asemenea, asistentul poate efectua cautari pe forum, asa ca " +
                         "daca iti doresti sa gasesti o postare spune-i cateva cuvinte cheie si o va gasi pentru tine.",
-                R.drawable.b, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.asistent_cautare, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
 
         // Harti
@@ -79,7 +95,9 @@ public class IntroTutorialSlider extends AppIntro2 {
         addSlide(AppIntroFragment.newInstance("Modul harti",
                 "Modulul harti iti permite sa vezi locuri de pescuit din jurul tau si sa " +
                         "interactionezi cu acestea.",
-                R.drawable.b, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.harta_general, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
 
         // Balta detaliata
@@ -87,14 +105,18 @@ public class IntroTutorialSlider extends AppIntro2 {
                 "Daca doresti mai multe informatii despre o balta, poti da click pe ea si " +
                         "vezi afla lucruri precum distanta pana la ea, ce pesti au mai fost prinsi acolo, chiar" +
                         " si review-uri de la alti oameni.",
-                R.drawable.b, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.harta_detaliata, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
         // Adaugare peste
         addSlide(AppIntroFragment.newInstance("Modul harti",
                 "Daca ai prins un peste si doresti sa il impartasesti cu ceilalti, poti face acest " +
                         "lucru prin optiunea de adaugare peste. Dupa ce completezi toate campurile, pestele " +
                         "va deveni vizibil pe balta unde a fost prins.",
-                R.drawable.b, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.harti_adaugare_peste, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
 
         // Profil
@@ -102,7 +124,9 @@ public class IntroTutorialSlider extends AppIntro2 {
         addSlide(AppIntroFragment.newInstance("Modul profil",
                 "Gestionarea contului persoanal se face de pe pagina de profil. Aici poti vedea " +
                         "diferite statistici rezultate din folosirea aplciatiei.",
-                R.drawable.b, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.profil_general, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
 
         // Modificare stergere cont
@@ -110,7 +134,9 @@ public class IntroTutorialSlider extends AppIntro2 {
                 "Modificarea si stergerea contului pot fi facute tot de pe aceasta pagina. " +
                         "Modificarea contului poate fi facuta oricand, insa stergerea contului este o actiune permanenta" +
                         " si ireversibila.",
-                R.drawable.b, ContextCompat.getColor(getApplicationContext(), R.color.purple_200)));
+                R.drawable.profil_actiuni, ContextCompat.getColor(getApplicationContext(), R.color.white),
+                ContextCompat.getColor(getApplicationContext(), R.color.black),
+                ContextCompat.getColor(getApplicationContext(), R.color.black)));
 
 
     }
